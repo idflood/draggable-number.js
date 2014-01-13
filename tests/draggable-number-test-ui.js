497c6732cc96c424d65a8c1ba0e6589d94401a7a
David Mignot <david.mig@gmail.com>
2014-01-13 13:37:59 +0100

    add tests for onMouseMove

--- tests/draggable-number-test-ui.js
+++ tests/draggable-number-test-ui.js
@@ -182,4 +182,49 @@ describe("Draggable-number (UI)", function() {
       document.body.style['user-select'].should.equal('all');
     });
   });
+
+  describe("DraggableNumberElement.onMouseMove", function() {
+    beforeEach(function() {
+      document.body.appendChild(input);
+      input.value = 10;
+      this.el = new DraggableNumber.Element(input);
+      this.lastMousePosition = {x: 0, y: 0}
+    });
+
+    afterEach(function() {
+      this.el.destroy();
+      delete this.el;
+      document.body.removeChild(input);
+    });
+
+    it("Should not modify value if mouse position is less than dragThreshold (10).", function() {
+      this.el.onMouseMove({clientX: 1, clientY: 0});
+      this.el.value.should.equal(10);
+    });
+
+    it("Should increment value by 1 if there is no modifier key.", function() {
+      this.el.onMouseMove({clientX: 20, clientY: 0});
+      this.el.value.should.equal(11);
+    });
+
+    it("Should increment value by 10 if shift key is pressed.", function() {
+      this.el.onMouseMove({clientX: 20, clientY: 0, shiftKey: true});
+      this.el.value.should.equal(20);
+    });
+
+    it("Should increment value by 0.1 if ctrl key is pressed.", function() {
+      this.el.onMouseMove({clientX: 20, clientY: 0, ctrlKey: true});
+      this.el.value.should.equal(10.1);
+    });
+
+    it("Should save new mouse position if difference is bigger than dragThreshold.", function() {
+      this.el.onMouseMove({clientX: 20, clientY: 0});
+      this.el.lastMousePosition.x.should.equal(20);
+    });
+
+    it("Should not save new mouse position if difference is smaller than dragThreshold.", function() {
+      this.el.onMouseMove({clientX: 9, clientY: 0});
+      this.el.lastMousePosition.x.should.equal(0);
+    });
+  });
 });
