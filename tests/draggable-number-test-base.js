6b38aab2565f5dcb40ecdb73341194cb28d7b001
David Mignot <david.mig@gmail.com>
2014-01-22 22:13:58 +0100

    add failing test for dragThresold option

--- tests/draggable-number-test-base.js
+++ tests/draggable-number-test-base.js
@@ -4,6 +4,30 @@ describe("Draggable-number (Base)", function() {
   var input = document.createElement("input");
   var input2 = document.createElement("input");
 
+  describe("DraggableNumberElement.dragThreshold", function() {
+    beforeEach(function() {
+      document.body.appendChild(input);
+      this.el = new DraggableNumber(input);
+    });
+
+    afterEach(function() {
+      this.el.destroy();
+      delete this.el;
+      document.body.removeChild(input);
+    });
+
+    it("By default dragThreshold should be 10", function() {
+      this.el._dragThreshold.should.equal(10);
+    });
+
+    it("By default dragThreshold should be modifiable from options", function() {
+      this.el.destroy();
+      this.el = new DraggableNumber(input, {dragThreshold: 42});
+      this.el._dragThreshold.should.equal(42);
+    });
+
+  });
+
   describe("DraggableNumberElement.getLargestDelta", function() {
     beforeEach(function() {
       document.body.appendChild(input);
