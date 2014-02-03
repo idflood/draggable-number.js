11f2e1aa49ad9e0b3a1a68a1a35ae2b4e37177d6
David Mignot <david.mig@gmail.com>
2014-02-03 22:37:47 +0100

    Test setting options from for input attributes, failing since not implemented

---
+++ tests/base/inputAttributes.js
@@ -0,0 +1,41 @@
+var should = chai.should();
+
+describe("DraggableNumber input attributes", function() {
+  beforeEach(function() {
+    this.input = document.createElement("input");
+    document.body.appendChild(input);
+    this.el
+  });
+
+  afterEach(function() {
+    this.el.destroy();
+    delete this.el;
+    document.body.removeChild(input);
+  });
+
+  it("It should be possible to set options with input attributes", function() {
+    this.input.setAttribute('data-min', "-17");
+    this.input.setAttribute('data-max', "2014");
+    this.input.setAttribute('data-dragThreshold', "24");
+    var el = new DraggableNumber(input);
+    el._min.should.equal(-17);
+    el._max.should.equal(2014);
+    el._dragThreshold.should.equal(24);
+    el.destroy();
+  });
+
+  it("Defining options should override data attributes", function() {
+    this.input.setAttribute('data-min', "-17");
+    this.input.setAttribute('data-max', "2014");
+    this.input.setAttribute('data-dragThreshold', "24");
+    var el = new DraggableNumber(input, {
+      min: 72,
+      max: 1234,
+      dragThreshold: 7
+    });
+    el._min.should.equal(72);
+    el._max.should.equal(1234);
+    el._dragThreshold.should.equal(7);
+    el.destroy();
+  });
+});
