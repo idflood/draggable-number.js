7eee463d54bbcdb56e72ab89e988b451f0dec98f
David Mignot <david.mig@gmail.com>
2014-02-04 06:45:55 +0100

    fix errors in input attributes test

--- tests/base/inputAttributes.js
+++ tests/base/inputAttributes.js
@@ -3,21 +3,19 @@ var should = chai.should();
 describe("DraggableNumber input attributes", function() {
   beforeEach(function() {
     this.input = document.createElement("input");
-    document.body.appendChild(input);
-    this.el
+    document.body.appendChild(this.input);
   });
 
   afterEach(function() {
-    this.el.destroy();
-    delete this.el;
-    document.body.removeChild(input);
+    document.body.removeChild(this.input);
+    delete this.input;
   });
 
   it("It should be possible to set options with input attributes", function() {
     this.input.setAttribute('data-min', "-17");
     this.input.setAttribute('data-max', "2014");
     this.input.setAttribute('data-dragThreshold', "24");
-    var el = new DraggableNumber(input);
+    var el = new DraggableNumber(this.input);
     el._min.should.equal(-17);
     el._max.should.equal(2014);
     el._dragThreshold.should.equal(24);
@@ -28,7 +26,7 @@ describe("DraggableNumber input attributes", function() {
     this.input.setAttribute('data-min', "-17");
     this.input.setAttribute('data-max', "2014");
     this.input.setAttribute('data-dragThreshold', "24");
-    var el = new DraggableNumber(input, {
+    var el = new DraggableNumber(this.input, {
       min: 72,
       max: 1234,
       dragThreshold: 7
